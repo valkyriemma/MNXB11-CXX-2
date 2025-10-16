8508c32144b56902cd2898a5e577cb73037e2d26
Emma <emmakrvaric@gmail.com>
2025-10-16 16:07:01 +0200

    attempted assignment 1.2 on my own. next step: write in int main

--- src/as1.cxx
+++ src/as1.cxx
@@ -1,5 +1,6 @@
 #include "as1.hpp"
 #include <iostream>
+#include <memory>
 
 namespace homework {
 
@@ -11,4 +12,22 @@ void swap_ptr(int* a, int* b, int *c) {
     *c = aOriginal; //chat told me to make C point to the ORIGINAL value of A, not the new value of A
     return;
 }
+
+    //As 1.2
+UniqueData::UniqueData(int value) {
+    // constructor
+    data_ = std::make_unique<int>(value);
+}
+
+int UniqueData::get() const {
+    // get value in heap
+    // use pointer? because "value" isnt known here
+    return *data_; 
+}
+
+void UniqueData::set(int value) {
+    // set value in heap
+    *data_ = value;
+}
+
 }; // namespace homework
