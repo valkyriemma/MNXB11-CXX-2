3a6b50e7ec239b013e6db8a506e1c10dd30490f1
Emma <emmakrvaric@gmail.com>
2025-10-16 14:59:37 +0200

    made first attempt at as1.1, then will check code with Chat to see if logic works, i think it will

--- src/as1.cxx
+++ src/as1.cxx
@@ -1,9 +1,23 @@
 #include "as1.hpp"
+#include <iostream>
 
 namespace homework {
 
-void swap_ptr(int* a, int* b, int *c) { return;}
+void swap_ptr(int* a, int* b, int *c) { 
+    *a = *b;
+    *b = *c;
+    *c = *a;
+    return;
+}
 
-
+int main(){
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    swap_ptr(&a, &b, &c);
+    std::cout << "a = " << a << '\n';
+    std::cout << "b = " << b << '\n';
+    std::cout << "c = " << c << '\n';
+}
 
 }; // namespace homework
